8e72380ca6914a7aa298b67c3a5ea427c0e30ba5
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-19 12:41:35 -0300

    search by name and id implemented

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import { Box, Center, HStack, Text } from "@chakra-ui/react";
 import { Icon } from '@iconify/react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Box bg="red.500" p={4} color={"white"}>
       <Center>
-        <HStack flexDirection={"row"}>
-          <Icon icon="mdi:pokeball" hFlip={true} fontSize={30}/>
-          <Text fontSize={"3xl"} mr={10}>Pokedex</Text>
-        </HStack>
+        <Link to="/">
+          <HStack flexDirection={"row"}>
+            <Icon icon="mdi:pokeball" hFlip={true} fontSize={30}/>
+            <Text fontSize={"3xl"} mr={10}>Pokedex</Text>
+          </HStack>
+        </Link>  
       </Center>
     </Box>
   )
--- src/components/ListComponent/ListComponent.tsx
+++ src/components/ListComponent/ListComponent.tsx
@@ -2,8 +2,20 @@ import { Search2Icon } from "@chakra-ui/icons";
 import { InputGroup, Box, InputLeftElement, Input, AbsoluteCenter, SimpleGrid, Center } from "@chakra-ui/react";
 import ListPokemon from "../../types/ListPokemon";
 import { PokemonCard } from "../PokemonCard";
+import { useEffect, useState } from "react";
 
 function ListComponent({ results }: ListPokemon) {
+
+  const [pokemonsList, setPokemonsList] = useState<Array<{name: string; url: string;}> | []>([]);
+  const [ searchPokemon, setSearchPokemon ] = useState<string>('');
+
+  useEffect(() => {
+    const filtereData = 
+    results?.filter((item) => item.name.toLowerCase().includes(searchPokemon.toLowerCase())||  
+      item.url.toLowerCase().includes(searchPokemon.toLowerCase()));
+    setPokemonsList(filtereData);
+  }, [searchPokemon, results]);
+
   return (
     <>
       <Box position={"relative"} bg="red.500" p={8} color={"white"} justifyContent={"center"}>
@@ -12,14 +24,21 @@ function ListComponent({ results }: ListPokemon) {
             <InputLeftElement pointerEvents='none'>
               <Search2Icon color='red.500'/>
             </InputLeftElement>
-            <Input color={"black"} type="text" width={"auto"} bg={"white"}/>
+            <Input 
+              color={"black"} 
+              type="text" 
+              width={"auto"} 
+              bg={"white"}
+              placeholder="Search"
+              onChange={(text) => setSearchPokemon(text.target.value)}
+            />
           </InputGroup>
         </AbsoluteCenter>
       </Box>
       <Center>
         <Box maxW={1400} marginTop={10} marginBottom={10}>
           <SimpleGrid  columns={5} gap={6}>
-            {results?.map((item) => (<PokemonCard key={item.name} {...item}/>))}
+            {pokemonsList?.map((item) => (<PokemonCard key={item.name} {...item}/>))}
           </SimpleGrid>  
         </Box>
       </Center>
